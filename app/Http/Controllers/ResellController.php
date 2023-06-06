81dc18e08a39dea534e6ae4db2dcce1ae8870a12
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-06 12:55:29 +0200

    123

--- app/Http/Controllers/ResellController.php
+++ app/Http/Controllers/ResellController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Imports\ResellImport;
 use App\Models\Customer;
 use App\Models\Resell;
 use App\Http\Requests\StoreResellRequest;
 use App\Http\Requests\UpdateResellRequest;
-use App\Imports\ResellsImport;
+
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -97,10 +99,101 @@ class ResellController extends Controller
     }
     public function import(Request $request)
     {
-        $file = $request->file('file');
+dd('ok');
+        $rules = [
+            'file' => 'required|mimes:csv,txt',
+        ];
 
-        Excel::import(new ResellsImport, $file);
+        $validator = \Validator::make($request->all(), $rules);
 
-        return redirect()->back()->with('success', 'CSV imported successfully.');
+        if($validator->fails())
+        {
+            $messages = $validator->getMessageBag();
+
+            return redirect()->back()->with('error', $messages->first());
+        }
+
+        $customers = (new ResellImport())->toArray(request()->file('file'))[0];
+
+        $totalCustomer = count($customers) - 1;
+        $errorArray    = [];
+        for($i = 1; $i <= count($customers) - 1; $i++)
+        {
+            $customer = $customers[$i];
+
+            $customerByEmail = Resell::where('email', $customer[2])->first();
+            if(!empty($customerByEmail))
+            {
+                $customerData = $customerByEmail;
+            }
+            else
+            {
+                $customerData = new Resell();
+                $customerData->customer_id      = $this->customerNumber();
+            }
+
+            $customerData->customer_id             = $customer[0];
+            $customerData->name             = $customer[1];
+            $customerData->email            = $customer[2];
+            $customerData->contact          = $customer[3];
+            $customerData->is_active        = 1;
+            $customerData->billing_name     = $customer[4];
+            $customerData->billing_country  = $customer[5];
+            $customerData->billing_state    = $customer[6];
+            $customerData->billing_city     = $customer[7];
+            $customerData->billing_phone    = $customer[8];
+            $customerData->billing_zip      = $customer[9];
+            $customerData->billing_address  = $customer[10];
+            $customerData->shipping_name    = $customer[11];
+            $customerData->shipping_country = $customer[12];
+            $customerData->shipping_state   = $customer[13];
+            $customerData->shipping_city    = $customer[14];
+            $customerData->shipping_phone   = $customer[15];
+            $customerData->shipping_zip     = $customer[16];
+            $customerData->shipping_address = $customer[17];
+            $customerData->balance          = 0;
+            $customerData->created_by       = \Auth::user()->creatorId();
+
+            if(empty($customerData))
+            {
+                $errorArray[] = $customerData;
+            }
+            else
+            {
+                $customerData->save();
+            }
+        }
+
+        $errorRecord = [];
+        if(empty($errorArray))
+        {
+            $data['status'] = 'success';
+            $data['msg']    = __('Record successfully imported');
+        }
+        else
+        {
+            $data['status'] = 'error';
+            $data['msg']    = count($errorArray) . ' ' . __('Record imported fail out of' . ' ' . $totalCustomer . ' ' . 'record');
+
+
+            foreach($errorArray as $errorData)
+            {
+
+                $errorRecord[] = implode(',', $errorData);
+
+            }
+
+            \Session::put('errorArray', $errorRecord);
+        }
+
+        return redirect()->back()->with($data['status'], $data['msg']);
     }
+//    public function import(Request $request)
+//    {
+//        $file = $request->file('file');
+//
+//        Excel::import(new ResellsImport, $file);
+//
+//        return redirect()->back()->with('success', 'CSV imported successfully.');
+//    }
 }
