4da5e7f7c73988b3c30bbb8b9004edcd5204773b
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-06 12:05:05 +0200

    Update menu.blade.php

--- resources/views/partials/admin/menu.blade.php
+++ resources/views/partials/admin/menu.blade.php
@@ -527,7 +527,9 @@
                                         </li>
                                     @endif
 
-                                           
+                                            <li class="dash-item ">
+                                                <a class="dash-link" href="{{ route('resell.index') }}">{{__('Resell')}}</a>
+                                            </li>
 
                                     @if(Gate::check('manage vender'))
                                         <li class="dash-item {{ (Request::segment(1) == 'vender')?'active':''}}">
