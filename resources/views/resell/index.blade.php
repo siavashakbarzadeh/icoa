a006c949348006b3c8ba1007d37e84a3580aaea6
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-06 13:03:41 +0200

    Update index.blade.php

--- resources/views/resell/index.blade.php
+++ resources/views/resell/index.blade.php
@@ -27,14 +27,14 @@ $profile=\App\Models\Utility::get_file('uploads/avatar/');
 
 @section('action-btn')
     <div class="float-end">
-        <a href="#" data-size="md"  data-bs-toggle="tooltip" title="{{__('Import')}}" data-url="{{ route('customer.file.import') }}" data-ajax-popup="true" data-title="{{__('Import customer CSV file')}}" class="btn btn-sm btn-primary">
+        <a href="#" data-size="md"  data-bs-toggle="tooltip" title="{{__('Import')}}" data-url="{{ route('resell.file.import') }}" data-ajax-popup="true" data-title="{{__('Import customer CSV file')}}" class="btn btn-sm btn-primary">
             <i class="ti ti-file-import"></i>
         </a>
-        <a href="{{route('customer.export')}}" data-bs-toggle="tooltip" title="{{__('Export')}}" class="btn btn-sm btn-primary">
+        <a href="{{route('resell.export')}}" data-bs-toggle="tooltip" title="{{__('Export')}}" class="btn btn-sm btn-primary">
             <i class="ti ti-file-export"></i>
         </a>
 
-        <a href="#" data-size="lg" data-url="{{ route('customer.create') }}" data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Create')}}" data-title="{{__('Create Customer')}}" class="btn btn-sm btn-primary">
+        <a href="#" data-size="lg" data-url="{{ route('resell.create') }}" data-ajax-popup="true" data-bs-toggle="tooltip" title="{{__('Create')}}" data-title="{{__('Create Customer')}}" class="btn btn-sm btn-primary">
             <i class="ti ti-plus"></i>
         </a>
     </div>
